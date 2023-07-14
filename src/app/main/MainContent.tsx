81c25e4e9352b23916191dec1227596bc227e13c
nakinton <r.nakinton@gmail.com>
2023-07-14 23:11:17 +0700

    chore: add page layout

---
+++ src/app/main/MainContent.tsx
@@ -0,0 +1,10 @@
+import Content from "./content";
+
+
+export const MainContent = () =>{
+    return(
+        <main><Content/></main>
+    )
+}
+
+export default MainContent;
---
+++ src/app/nav/Navigation.tsx
@@ -0,0 +1,16 @@
+
+const Navigation = () => {
+  return (
+    <nav className="left-sidebar">
+      <div className="left-sidebar-item-group">
+        <div className="left-sidebar-item">Nav</div>
+        <div className="left-sidebar-item">Nav</div>
+        <div className="left-sidebar-item">Nav</div>
+        <div className="left-sidebar-item">Nav</div>
+        <div className="left-sidebar-item">Nav</div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navigation
