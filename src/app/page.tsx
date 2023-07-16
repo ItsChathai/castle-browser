5c23735bc4614b760fae7007480733c706d56188
nakinton <r.nakinton@gmail.com>
2023-07-16 18:32:53 +0700

    refactor: improve routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 import './globals.css'
-import MainContent from './main/MainContent'
-import Navigation from './nav/Navigation'
 export default function Home(){
-  return <div className="page-container">
-      <Navigation/>
-      <MainContent/>
-  </div>
+  return <>main</>
 }
